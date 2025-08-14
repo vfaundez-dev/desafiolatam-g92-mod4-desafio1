0ac31f0c1845da7dfafb2d252ceeb246c8ead3e9
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-08-14 12:02:15 -0400

    Actualizado token

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const UserProvider = ({ children }) => {
 
-  const [token, setToken] = useState(false);
+  const [token, setToken] = useState(true);
   const [user, setUser] = useState({
     name: 'Vladimir Faundez',
     email: 'correo@correo.com'
